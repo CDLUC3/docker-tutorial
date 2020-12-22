7a06125de1b21470a9d390fca5253835a46badf3
Terry Brady <terrywbrady@gmail.com>
2020-12-22 14:15:34 -0800

    java code

--- examples/session1/java/src/main/java/org/cdluc3/IngestCLI.java
+++ examples/session1/java/src/main/java/org/cdluc3/IngestCLI.java
@@ -4,28 +4,52 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.Scanner;
 
 public class IngestCLI {
-    public static final void main(String[] argv) {
-        System.out.println("Hello..");
+    private String connstr = "jdbc:mysql://mydb:3306/userdb";
+    private String user = "root";
+    private String password = "password";
 
-        try {  
-            Connection con = DriverManager.getConnection(  
-                "jdbc:mysql://mydb:3306/userdb",
-                "root",
-                "password"
-            );  
-            String sql = "select 1, now(), user()";
-            System.out.println(sql);
+    public IngestCLI() {
+    }
+
+    public void runQuery(String sql) throws SQLException {
+        try(Connection con = DriverManager.getConnection(connstr, user, password)){
             Statement stmt = con.createStatement();  
             ResultSet rs=stmt.executeQuery(sql);  
             while(rs.next()) {
                 System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-            } 
-            con.close();  
+            }     
+        }
+    }
+
+    public void processInput(String fname) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fname))) {
+            for(String line = br.readLine(); line != null; line = br.readLine()) {
+                System.out.println(line);
+            }
         } catch(Exception e) { 
             System.out.println(e);
         }  
+    }
+
+    public void processInput1() {
+        try {  
+            runQuery("select 1, now(), user()");
+        } catch(Exception e) { 
+            System.out.println(e);
+        }  
+    }
+
+    public static final void main(String[] argv) {
+        System.out.println("Hello..");
+        IngestCLI cli = new IngestCLI();
+        cli.processInput("test.csv");
+
         System.out.println("...");
     }
 
